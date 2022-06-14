be90838374b9d5dc74963b77c0af0bdc9b3a37dd
Jihyun247 <wlguslove12345@gmail.com>
2022-06-14 02:45:12 +0000

    ✨ NetworkResult 추상팩토리 활용하여 Switch 통일

--- DaangnMarket/DaangnMarket/Scene/Network/NetworkResult.swift
+++ DaangnMarket/DaangnMarket/Scene/Network/NetworkResult.swift
@@ -5,7 +5,7 @@
 //  Created by 김지현 on 2022/05/31.
 //
 
-import Foundation
+import UIKit
 
 enum NetworkResult<T> {
     case success(T?)
@@ -13,4 +13,64 @@ enum NetworkResult<T> {
     case serverErr
     case pathErr
     case networkFail
+}
+
+protocol NetworkResultProtocol {
+    func resultMethod()
+}
+
+struct Success<T: Codable>: NetworkResultProtocol {
+
+    var response: T?
+    func resultMethod() {
+        print("성공")
+    }
+}
+
+struct RequestErr: NetworkResultProtocol {
+    
+    var errorMessage: String
+    func resultMethod() {
+        print(errorMessage)
+    }
+}
+
+struct ServerErr: NetworkResultProtocol {
+    
+    func resultMethod() {
+        print("servererror")
+    }
+}
+
+struct PathErr: NetworkResultProtocol {
+
+    func resultMethod() {
+        print("patherr")
+    }
+}
+
+struct NetworkFail: NetworkResultProtocol {
+
+    func resultMethod() {
+        print("네트워크fail")
+    }
+}
+
+struct NetworkResultFactory {
+    
+    static func makeResult<T: Codable>(resultType: NetworkResult<T>) -> NetworkResultProtocol {
+        switch resultType {
+        case .success(let response):
+            return Success(response: response)
+        case .requestErr(let message):
+            return RequestErr(errorMessage: message)
+        case .serverErr:
+            return ServerErr()
+        case .pathErr:
+            return PathErr()
+        case .networkFail:
+            return NetworkFail()
+        }
+    }
+    
 }
